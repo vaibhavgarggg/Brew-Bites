f7c85824ba129844888804e75822e51d955509de
Vinit Upadhyay <vinitupadhyay994@gmail.com>
2025-04-23 09:16:02 +0530

    adding remaining photos

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -55,7 +55,7 @@ const menuItems = [
         name: "French Toast",
         price: 11.00,
         category: "breakfast",
-        image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=500&q=80",
+        image: "https://images.unsplash.com/photo-1595044643502-616eeebbdff3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description: "Golden brown French toast with syrup"
     },
     {
