5db5975928d2126460e4e703f83c146bd09e5beb
Vinit Upadhyay <vinitupadhyay994@gmail.com>
2025-04-28 01:51:32 +0530

    removing unnecessary pages

--- JavaScript/products.js
+++ JavaScript/products.js
@@ -51,7 +51,7 @@ export const products = [
     description: 'Sourdough toast topped with mashed avocado, cherry tomatoes, and microgreens.',
     price: 8.99,
     category: 'breakfast',
-    image: 'https://images.unsplash.com/photo-1603046891744-76e6300255c0?ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80',
+    image: 'https://images.unsplash.com/photo-1608363831438-06d9293864c9?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     popular: true
   },
   {
